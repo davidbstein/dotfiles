c2f71ed65f8fe7068a7e471f5bcbaadf92a2dc9e
David Stein <davidbstein@gmail.com>
2014-06-06 18:19:29 +0000

    fix typo

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 mv ~/dotfiles ~/.dotfiles
 
-for dotfile in $(ls ~/dotfiles)
+for dotfile in $(ls ~/.dotfiles)
 do 
     rm -rf ~/.$dotfile
-    ln -s ~/dotfiles/$dotfile ~/.$dotfile
+    ln -s ~/.dotfiles/$dotfile ~/.$dotfile
 done 
 exec bash
